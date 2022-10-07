0c1ec455c19b69582873170530fa87ae779b30bd
PetrDoroshev <doroshevpetr@gmail.com>
2022-10-07 14:03:19 +0300

    pl/pgsql task

--- pl/pgsql/tasks.sql
+++ pl/pgsql/tasks.sql
@@ -8,14 +8,33 @@ $$ LANGUAGE plpgsql;
 --2
 DO $$
 BEGIN
-
     RAISE NOTICE '%', current_date;
-
 END
 $$ LANGUAGE plpgsql;
 
 --3
+DO $$
+DECLARE
+    a INT;
+    b INT;
+BEGIN
+    a = 5;
+    b = 4;
+    RAISE NOTICE 'a + b = %', a + b;
+    RAISE NOTICE 'a - b = %', a - b;
+    RAISE NOTICE 'a * b = %', a * b;
+END
+$$ LANGUAGE plpgsql;
 
+--4
+
+DO $$
+DECLARE
+    mark INT = 4;
+BEGIN
+    
+END
+$$ LANGUAGE plpgsql;
 
 
 
